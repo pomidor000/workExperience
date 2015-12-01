af5653e50f094c48107e4e9037bc61ad78c5ccb6
DLights <DLights@DLights-PC>
2015-12-01 16:55:49 +0000

    session store additor, to add the sessions on multiple pages

---
+++ src/java/ie/zenit/WorkingExperience/SessionStore.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.zenit.WorkingExperience;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author DLights
+ */
+public class SessionStore {
+    
+    private HttpSession session = null;
+    
+    private HttpSession getSession() {
+        if (this.session == null) {
+            // get current faces instance for this request
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            // get current user session (or create one)
+            this.session = (HttpSession)facesContext.getExternalContext().getSession(true);
+        }
+        
+        return this.session;
+    }
+
+    public Object get(String key) {
+        return this.getSession().getAttribute(key);
+    }
+   
+    public void put(String key, Object value) {
+        this.getSession().setAttribute(key, value);
+    }
+}
