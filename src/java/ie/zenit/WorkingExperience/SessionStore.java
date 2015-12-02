8ec7fd52f01032a35f70f01f9b3a1c1303afc33f
DLights <DLights@192.168.0.3>
2015-12-02 17:45:34 +0000

    i added some comments, for better readability.

--- src/java/ie/zenit/WorkingExperience/SessionStore.java
+++ src/java/ie/zenit/WorkingExperience/SessionStore.java
@@ -1,20 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ie.zenit.WorkingExperience;
 
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author DLights
- */
+// this method basicaly store the session for all pages, then the user can use the webpages to work in application without 
+// worrying if the user is logged on, or logged off
 public class SessionStore {
     
-    private HttpSession session = null;
+    private HttpSession session = null; // assigning the variable to null in default
     
     private HttpSession getSession() {
         if (this.session == null) {
@@ -25,13 +19,13 @@ public class SessionStore {
         }
         
         return this.session;
-    }
+    } // it is checking if the session is active (!= to null), and set the faces
 
     public Object retrieve(String key) {
-        return this.getSession().getAttribute(key);
-    }
+        return this.getSession().getAttribute(key); // this method is storing the key variable(usefull in other methods) 
+    }                                               // and returning its value
     
     public void store(String key, Object value) {
-        this.getSession().setAttribute(key, value);
+        this.getSession().setAttribute(key, value); // the method is storing the key and value variable to be used in other methods
     }
 }
